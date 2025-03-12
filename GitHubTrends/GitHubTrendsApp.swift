b5d24bb9e77ece0177321a253b31365f639fc829
Valery Trakala <vtrakala@delhaize.be>
2025-03-12 16:24:45 +0100

    Initial Commit

---
+++ GitHubTrends/GitHubTrendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTrendsApp.swift
+//  GitHubTrends
+//
+//  Created by Valery Trakala on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTrendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
